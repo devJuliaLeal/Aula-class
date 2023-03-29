fd440cf2f9c7aff0292385e4df1e984163ac5cec
juedu36 <120651477+devJuliaLeal@users.noreply.github.com>
2023-03-29 10:34:30 -0300

    adding files

---
+++ programaParaSomar001.c
@@ -0,0 +1,37 @@
+/* =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^= =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=
+    Autor: Julia
+    Data: 29 de Março de 2023
+=^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^= =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=*/
+
+/*  Bibliotecas */
+#include <stdio.h>
+#include <stdlib.h>
+
+// =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=   =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=
+/* --- Função Principal --- */
+main()
+{
+
+    int num1,
+        num2,
+        res;
+
+    printf("Digite o primeiro numero:");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo numero:");
+    scanf("%d", &num2);
+
+    res = num1 + num2;
+
+    printf("%d + %d = %d\n", num1, num2, res);
+
+    system("PAUSE");
+    return 0;
+}
+/* end main */
+/*
+
+*/
+/* =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^= =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^= */
+/* --- Final do Programa --- */
