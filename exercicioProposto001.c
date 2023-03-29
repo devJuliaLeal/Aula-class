ca5ed67175a76b7f9d032f48ad394b8936f25928
juedu36 <120651477+devJuliaLeal@users.noreply.github.com>
2023-03-29 11:01:14 -0300

    create exec

--- exercicioProposto001.c
+++ exercicioProposto001.c
@@ -13,23 +13,23 @@
 /* --- Função Principal --- */
 main()
 {
-    float pi = 3.1415,
-          raio = 0.0,
-          altura = 0.0,
-          volume;
+    int pi = 3.1415,
+        numero1 = 0.0,
+        numero2 = 0.0,
+        subtracao = 0.0,
+        multiplicacao = 0.0,
+        divisao = 0.0;
 
-printf("Digite o raio do cilindro em metros:");
-    scanf("%f", &raio);
-printf("Digite o altura do cilindro em metros:");
-    scanf("%f", &altura);
+    printf("Digite um numero real: ");
+    scanf("%d", &numero1);
+    printf("Digite outro numero real: ");
+    scanf("%d", &numero2);
 
-volume = pi * raio * raio * altura; 
-printf()
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = numero1 / numero2;
 
-
-
-
-
+    printf("A subtracao e %d , a multiplicacao e %d , e a divisao e %d ", subtracao, multiplicacao, divisao);
 
     system("PAUSE");
     return 0;
---
+++ novoexercicioProposto001.c
@@ -0,0 +1,40 @@
+/*  =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=   =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=
+    Autor: Julia
+    Data:  Março de 2023
+
+ =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=   =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=*/
+
+/* =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=   =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^= */
+/* --- Bibliotecas --- */
+#include <stdio.h>
+#include <stdlib.h>
+
+/*  =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=   =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=*/
+/* --- Função Principal --- */
+main()
+{
+    float numero1 = 0.0,
+          numero2 = 0.0,
+          subtracao = 0.0,
+          multiplicacao = 0.0,
+          divisao = 0.0;
+
+    printf("Digite um numero real: ");
+    scanf("%f", &numero1);
+    printf("Digite outro numero real: ");
+    scanf("%f", &numero2);
+
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = numero1 / numero2;
+
+    printf("A subtracao e %.2f , a multiplicacao e %.2f , e a divisao e %.2f\n", subtracao, multiplicacao, divisao);
+
+    printf("%.2f - %.2f = %.2f\n", numero1, numero2, subtracao);
+    printf("%.2f * %.2f = %.2f\n", numero1, numero2, multiplicacao);
+    printf("%.2f / %.2f = %.2f\n", numero1, numero2, divisao);
+
+} /* end main */
+
+/*  =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=   =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=    =^..^=*/
+/* --- Final do Programa --- */
